a33727665f46dcf4b4ca4e2ff3542faaf83eff67
Tim-Hopanchuk <tim.hpnk@gmail.com>
2025-09-15 21:30:14 +0300

    feat: implement getEmptyField

---
+++ src/utils/getEmptyField.ts
@@ -0,0 +1,26 @@
+import type { CellType } from '@/utils/types';
+
+export function getEmptyField(width: number, height: number): CellType[][] {
+  if (width < 1 || height < 1 || width * height < 2) {
+    throw new Error("Invalid field's width or height");
+  }
+
+  const emptyField: CellType[][] = [];
+
+  for (let y = 0; y < height; y++) {
+    emptyField[y] = [];
+    for (let x = 0; x < width; x++) {
+      emptyField[y][x] = {
+        isOpened: false,
+        isMine: false,
+        isFlagMarked: false,
+        isQuestionMarked: false,
+        isExploded: false,
+        adjacentMines: 0,
+        index: [y, x],
+      };
+    }
+  }
+
+  return emptyField;
+}
