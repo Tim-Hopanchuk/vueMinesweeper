32fea4535c1dd2d8d8922436f3bd8f76a44b2f44
Tim-Hopanchuk <tim.hpnk@gmail.com>
2025-09-23 17:03:38 +0300

    feat: implement Difficulty component

---
+++ src/components/Difficulty.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  difficulty: 'easy' | 'medium' | 'hard';
+}>();
+const emits = defineEmits<{
+  changeDifficulty: [difficulty: 'easy' | 'medium' | 'hard'];
+}>();
+</script>
+
+<template>
+  <button @click="emits('changeDifficulty', props.difficulty)">{{ props.difficulty }}</button>
+</template>
+
+<style scoped>
+button {
+  height: 34px;
+  width: 100px;
+  padding: 0px;
+  margin: 0px;
+}
+</style>
