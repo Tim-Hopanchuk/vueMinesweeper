4031cd55570d95193e365d0b4add0a899eedbff6
Tim-Hopanchuk <tim.hpnk@gmail.com>
2025-09-30 13:06:13 +0300

    feat: implement Dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { SmileStateType } from '../utils/types';
+import Smile from './Smile.vue';
+
+const props = defineProps<{
+  remainingMines: number;
+  elapsedTime: number;
+  smileState: SmileStateType;
+}>();
+const emits = defineEmits(['resetGame']);
+</script>
+
+<template>
+  <div class="game-header border-in-thick">
+    <div class="counter-container border-in-thin">{{ props.remainingMines }}</div>
+    <Smile :smileState="props.smileState" @click="emits('resetGame')" />
+    <div class="counter-container border-in-thin">{{ props.elapsedTime }}</div>
+  </div>
+</template>
+
+<style scoped></style>
